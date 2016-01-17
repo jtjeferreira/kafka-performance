93d304bf6b9dd13645b8070d1631f39cbdbe5f6c
João Ferreira <jtjeferreira@gmail.com>
2016-01-17 20:06:47 +0000

    improve run scripts

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-times=$1
+for i in {1,10,100,1000,10000,100000}; do
+  sbt "test:runMain com.miguno.kafkastorm.integration.KafkaConsumerMain $i"
+  sleep 1
+  sbt "test:runMain com.miguno.kafkastorm.integration.ReactiveKafkaConsumerMain $i"
+  sleep 1
 
-sbt "test:runMain com.miguno.kafkastorm.integration.KafkaConsumerMain $times"
-sleep 1
-sbt "test:runMain com.miguno.kafkastorm.integration.ReactiveKafkaConsumerMain $times"
-sleep 1
-
-sbt "test:runMain com.miguno.kafkastorm.integration.KafkaConsumerProducerMain $times"
-sleep 1
-sbt "test:runMain com.miguno.kafkastorm.integration.ReactiveKafkaConsumerProducerMain $times"
-sleep 1
+  sbt "test:runMain com.miguno.kafkastorm.integration.KafkaConsumerProducerMain $i"
+  sleep 1
+  sbt "test:runMain com.miguno.kafkastorm.integration.ReactiveKafkaConsumerProducerMain $i"
+  sleep 1
+done
--- run2.sh
+++
@@ -1,7 +0,0 @@
-times=$1
-watermark=$2
-
-sbt "test:runMain com.miguno.kafkastorm.integration.KafkaConsumerProducerMain $times"
-sleep 1
-sbt "test:runMain com.miguno.kafkastorm.integration.ReactiveKafkaConsumerProducerMain $times $watermark"
-sleep 1
